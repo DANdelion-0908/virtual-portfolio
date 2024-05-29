18169ce616bf154117436ac1bab7231a36081ddf
DANdelion <pen22217@uvg.edu.gt>
2024-05-28 21:22:13 -0600

    Update MainCard.jsx

--- app/components/MainCard/MainCard.jsx
+++ app/components/MainCard/MainCard.jsx
@@ -10,7 +10,7 @@ const MainCard = () => {
     {
       "title": "Genshin Excellen't",
       "description": "Un proyecto en desarrollo que tiene como objetivo facilitar llevar el registro de lanzamientos realizados en el videojuego Genshin Impact.",
-      "image": "/imgs/Genshin_ex.png",
+      "image": "imgs/Genshin_ex.png",
       "link": "https://github.com/DANdelion-0908/genshin_excellent.git",
       "techs": [
         "/techs/next.png",
@@ -23,7 +23,7 @@ const MainCard = () => {
     {
       "title": "Javascript Only Chat",
       "description": "Un chat desarrollado únicamente con Javascript y hosteado en un servidor, lo que permite enviar y recibir mensajes con otros usuarios.",
-      "image": "/imgs/js_only.png",
+      "image": "imgs/js_only.png",
       "link": "https://github.com/DANdelion-0908/JavaScript-Only.git",
       "techs": [
         "/techs/javascript.png",
@@ -34,7 +34,7 @@ const MainCard = () => {
     {
       "title": "Simulador de movimiento de electrones",
       "description": "Un simulador desarrollado como solución a un problema de parcial de física. Es posible modificar el entorno y apreciar el desplazamiento de lso electrones dentro de un cable. También posee una opción con la cual se simula una caminata aleatoria realizada por un electrón. .",
-      "image": "/imgs/simulator.png",
+      "image": "imgs/simulator.png",
       "link": "https://github.com/DANdelion-0908/Problema5_Parcial3.git",
       "techs": [
         "/techs/python.png",
@@ -44,7 +44,7 @@ const MainCard = () => {
     {
       "title": "Sabor",
       "description": "Una aplicación web desarrollada para ser utilizada por un restaurante. Cuenta con opciones para generar reportes y facturas, abrir y cerrar cuentas, entre otras.",
-      "image": "/imgs/sabor.png",
+      "image": "imgs/sabor.png",
       "link": "https://github.com/chuy-zip/proyecto2_BD1.git",
       "techs": [
         "/techs/javascript.png",
@@ -57,7 +57,7 @@ const MainCard = () => {
     {
       "title": "Copython",
       "description": "Una aplicación para Android que tiene como propósito enseñar a cualquier persona a programar en Python con guías, ejercicios y explicaciones..",
-      "image": "/imgs/copython.png",
+      "image": "imgs/copython.png",
       "link": "https://github.com/chuy-zip/Proyecto_PPM_Copython.git",
       "techs": [
         "/techs/kotlin.png"
@@ -66,9 +66,9 @@ const MainCard = () => {
   ]
 
   const achievements = [
-    "/imgs/diploma.png",
-    "/imgs/robot.jpg",
-    "/imgs/title.png"
+    "imgs/diploma.png",
+    "imgs/robot.jpg",
+    "imgs/title.png"
   ]
 
   return (
