173f82c3a7fb7530580bdd84955ae1b3a0c5d8c7
DANdelion <pen22217@uvg.edu.gt>
2024-05-28 22:40:30 -0600

    Update MyHero.jsx

--- app/components/MyHero/MyHero.jsx
+++ app/components/MyHero/MyHero.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const MyHero = () => {
   return (
     <section id='info' className='pt-20'>
-        <div className="hero h-auto w-[100%] bg-base-100 rounded-lg mx-auto">
+        <div className="hero h-auto w-[65%] bg-base-100 rounded-lg mx-auto text-justify">
         <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src="yo.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-            <div>
+            <img src="yo.jpg" className="max-w-sm max-h-[500px] rounded-lg shadow-2xl" />
+            <div className='mr-14'>
             <h1 className="text-5xl font-bold">¿Quién soy?</h1>
-            <p className="py-6">Me considero una persona capaz de adaptarse a diversas situaciones y con la <br />
-                capacidad de aprender a realizar tareas que me parecen nuevas o me sacan de mi <br />
-                zona de confort, siempre buscando poder completarlas y entregar un trabajo de <br />
-                calidad. Me gusta trabajar ya sea indivdual o colectivamente y mantener un <br />
-                ambiente positivo y con buena comunicación entre mis colegas. Soy fan de <br />
-                probar nuevas experiencias y aprender de ellas con elobjetivo de aprender <br />
+            <p className="py-6">Me considero una persona capaz de adaptarse a diversas situaciones y con la
+                capacidad de aprender a realizar tareas que me parecen nuevas o me sacan de mi
+                zona de confort, siempre buscando poder completarlas y entregar un trabajo de
+                calidad. Me gusta trabajar ya sea indivdual o colectivamente y mantener un
+                ambiente positivo y con buena comunicación entre mis colegas. Soy fan de
+                probar nuevas experiencias y aprender de ellas con elobjetivo de aprender
                 y aumentar mi conocimiento.</p>
             </div>
         </div>
